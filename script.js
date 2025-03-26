d993eedb9a4e9570c8fb41e99c9dce423b0c24b3
ShrijithSM <shrijithsmenon@gmail.com>
2025-03-26 15:18:11 +0530

    new model

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
